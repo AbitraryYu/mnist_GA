ede3a00f568ac94bfc2e61784386957b1edc5432
AbitraryYu <46883359+AbitraryYu@users.noreply.github.com>
2021-12-03 17:55:35 +0800

    Update genetic and predict

--- predict.py
+++ predict.py
@@ -18,7 +18,7 @@ X_train = X_train.astype('float32')
 
 X_train /= 255
 
-model = load_model("cnn.h5")
+model = load_model("cnn0.h5")
 
 output = model.predict(X_train[0].reshape(1,feature_vector_length))
 
